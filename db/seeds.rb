1b6278513172c280da606f4f82691a3d3ea156eb
Austin <austinchoi22@gmail.com>
2021-12-03 15:05:14 -0700

    basic seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Category.destroy_all
+
+games = Category.create(name: "Games")
+shirts = Category.create(name: "Shirts")
+posters = Category.create(name: "Posters")
+
+games.items.create(name: "PokeMon", description: "Catch Em All", price: 59.99)
+shirts.items.create(name: "Hey Arnold", description: "Football Head", price: 39.99)
+posters.items.create(name: "Grateful Dead", description: "Hippies Man", price: 4.99)
+
+puts "seeded#{Category.all.length} categories"
