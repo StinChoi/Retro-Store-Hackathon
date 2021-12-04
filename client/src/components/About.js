be0164e5f996772069135d05fadbf8abd615f681
Fabio Xavier <fabio.g.xavier33@gmail.com>
2021-12-04 16:35:53 -0700

    pic

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -9,9 +9,11 @@ const About = () => {
       <h2>Founders of Everything Retro</h2>
       <br />
       <h3>Fabio Xavier</h3>
-      <p></p>
+      <img src="https://lh3.googleusercontent.com/4cPtvE2YJT3qjrYEL3BNlYQHmxyYwt2QBYCPaoOzgnCD74voM4ORA8M6oy7KDa_pvOReyWmc0PVyPeTeBV-M6lotMwYaXgf8fch2SDcSuRDpHU1QQvzcdC5R53l1JMA7-qZGN8fsktKVbK5-dwISaZYEVRiSpjwLt4XAnTm8Smh0IETHWQuofHMQ_wjyAOn5dpIpSg8-Nf7QJgXLxwEw3LgHnPQTrWpWVOEc6zjsAeQ5mbo0yqCcK5og9moajAdg-NwQyeVeBvfB1RTvYfPnYa2Eg3KG_x462rwbp-KMOjbLujxW2Plj-nzNKAlHW_1AOkMKZjFMI34CO3mVZP2XyhOlbzcLF7YCuTx_Snv8SGAkNQDewtlrFx7ecjcjq__zi4rPY4jW_h4j5bBWauRb11-fwdgDswPWIJf4uNL7Q19GQVEjK6OiLo58EzYpY95xflj_GshBGiDM9rZ6v4KOLfGIvEoBvG_8fCGeGBamxu9iNVGi3AlRNd2rvIdk_RBy_DBRyE4_clEW_1kWxggUtniM1gGDNE8Qj8XYwS75y1NRW2xt1zySUId4tzfEx_3hVRPSVBeiaaEFFhRReLh3BXMTe5jHhFRt-w7zoA46kmuyPykykA34t-WQOoWv5fn8mzwzYzcmUVbZ9Lt0XFbCoBtclHvu8IaefCRSh3w8OtfhUk70SuO3UcUc9mRhgjWtgdDHt2_d2nUHfNxCjCca8Ps=w1722-h1875-no?authuser=0" alt="picture of shego" width="300px"/>
+      <p>Born in Brazil. Fabio loves illustration and comic books. He has currently published 2 webcomics on WEBTOONS. He owns a cat named Shego.</p>
       <br />
       <h3>Sky Lin</h3>
+      <img src="" alt="picture of pet" width="300px"/>
       <p>Born and raised in Guangzhou, China. Has a dog named Kuma.</p>
       <br />
       <h3>Austin Choi</h3>
