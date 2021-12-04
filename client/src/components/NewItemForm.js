6bc98e1ddc7448e626e63e71c55877b2e46bea57
Austin <austinchoi22@gmail.com>
2021-12-04 12:07:08 -0700

    added new item form

--- client/src/components/NewItemForm.js
+++ client/src/components/NewItemForm.js
@@ -1,4 +1,43 @@
 import axios from "axios";
-import React, {useState} from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router";
 
-const 
+const ItemNew = () => {
+
+  const navigate = useNavigate();
+
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState("");
+  const [description, setDescription] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const newItem = {
+      name: name,
+      price: price,
+      description: description,
+    };
+    await axios.post("/api/items", newItem);
+    navigate("/items");
+  };
+
+  return (
+    <div>
+      <h1>Add New Item Below</h1>
+      <div>
+        <form onSubmit={handleSubmit}>
+          <p>Item Name:</p>
+          <input value={name} onChange={(e) => setName(e.target.value)} />
+          <p>Item Price:</p>
+          <input value={price} onChange={(e) => setPrice(e.target.value)} />
+          <p>Item Description:</p>
+          <input value={description} onChange={(e) => setDescription(e.target.value)} />
+          <button>Create New Item</button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default ItemNew;
