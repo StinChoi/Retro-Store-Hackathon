78edb01399eea974084e3a69e9ba5081a5a11d3d
Fabio Xavier <fabio.g.xavier33@gmail.com>
2021-12-03 17:23:49 -0700

    added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,14 +5,35 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+prevCats = Category.all.length
+prevJobs = Jobs.all.length
 Category.destroy_all
+Jobs.destroy_all
 
-games = Category.create(name: "Games")
-shirts = Category.create(name: "Shirts")
-posters = Category.create(name: "Posters")
+puts "#{prevCats} categories cleared"
+puts "#{prevJobs} jobs cleared"
 
-games.items.create(name: "PokeMon", description: "Catch Em All", price: 59.99)
-shirts.items.create(name: "Hey Arnold", description: "Football Head", price: 39.99)
-posters.items.create(name: "Grateful Dead", description: "Hippies Man", price: 4.99)
+item_names = [Faker::Commerce.product_name, Faker::Game.title, Faker::Movie.title]
+
+10.times do 
+    category = Category.create(name: Faker::Commerce.department)
+    10.times do
+        category.items.create(
+            name: item_names.sample,
+            description: Faker::ChuckNorris.fact,
+            price: Faker::Commerce.price,
+        )
+    end
+end
+
+# games = Category.create(name: "Games")
+# shirts = Category.create(name: "Shirts")
+# posters = Category.create(name: "Posters")
+
+# games.items.create(name: "PokeMon", description: "Catch Em All", price: 59.99)
+# shirts.items.create(name: "Hey Arnold", description: "Football Head", price: 39.99)
+# posters.items.create(name: "Grateful Dead", description: "Hippies Man", price: 4.99)
 
 puts "seeded#{Category.all.length} categories"
