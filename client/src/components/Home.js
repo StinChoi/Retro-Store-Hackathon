84c3c2913249823c4a2a1dce298dd1810204b036
Austin <austinchoi22@gmail.com>
2021-12-04 10:57:02 -0700

    Items and Item set up

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,9 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
     <div>
-      <h1>Home</h1>
+      <h1>Welcome to Everything Retro</h1>
+      <Link to="/categories">Enter Shop</Link>
+      <br />
+      <Link to="/jobs">Careers</Link>
+      <br />
+      <Link to="/about">About</Link>
     </div>
   )
 }
