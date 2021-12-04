683d36376f6e6467867c5876968adcb959ef97e8
Fabio Xavier <fabio.g.xavier33@gmail.com>
2021-12-04 14:50:14 -0700

    quick navbar styling

--- client/src/components/Item.js
+++ client/src/components/Item.js
@@ -1,12 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import { useLocation, useParams } from "react-router";
 import { Link } from "react-router-dom";
+import ItemEdit from "./EditItemForm";
 
 const Item = () => {
 
+  const [showNewForm, setShowNewForm] = useState(false);
   const location = useLocation();
   const {item} = location.state;
   const {category_id, id} = useParams();
+
+  const toggleNewForm = () => {
+    setShowNewForm(!showNewForm);
+  };
 
   return (
     <div>
@@ -14,6 +20,8 @@ const Item = () => {
       <p>Name: {item.name}</p>
       <p>Price: {item.price}</p>
       <p>Description: {item.description}</p>
+      <button onClick = {toggleNewForm}>{showNewForm ? "Cancel" : "Edit Item"}</button>
+      {showNewForm && <ItemEdit item={item} toggleNewForm={toggleNewForm}/>}
     </div>
   );
 };
