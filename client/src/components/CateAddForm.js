5ebf79b1b5e61053799293b4eb59e821120d46dd
Sky Lin <linsky519@gmail.com>
2021-12-04 11:04:55 -0700

    Categories completed.

---
+++ client/src/components/CateAddForm.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const CateAddForm = () => {
+  return (
+    <div>
+      <h1>Add New Category</h1>
+    </div>
+  );
+};
+
+export default CateAddForm;
--- client/src/components/Categories.js
+++ client/src/components/Categories.js
@@ -1,11 +1,64 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 const Categories = () => {
+  const [showNewForm, setShowNewForm] = useState(false);
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    getCategories();
+  },[]);
+
+  const getCategories = async () => {
+    let res = await axios.get("/api/categories");
+    setCategories(res.data)
+  };
+
+  const deleteCategory = async (id) => {
+    await axios.delete(`/api/categories/${id}`);
+    setCategories(categories.filter((c) => c.id !== id));
+  }
+
+  const toggleNewForm = () => {
+    setShowNewForm(!showNewForm);
+  };
+
+  // const addCategory = (category) => {
+  //   setCategories([category, ...categories]);
+  // };
+
+  const renderCategories = () => {
+    if(categories.length === 0){
+      return <h2>No Categories</h2>
+    }
+    else {
+      return categories.map((category) => {
+        const renderItems = () => {
+          return category.items.map((item) => 
+            // <p key = {item.id}>Item: {item.name}</p>
+            item.name
+          );
+        };
+        return(
+          <div key = {category.id}>
+            <p>Name: {category.name}</p>
+            {renderItems()}
+            <br />
+            <button onClick = {() => deleteCategory(category.id)}>DELETE</button>
+          </div>
+          );
+      });
+    }
+  };
+
   return (
     <div>
       <h1>Categories</h1>
+      <button onClick = {toggleNewForm}>{showNewForm ? "Cancel" : "New Category"}</button>
+      {/* {showNewForm && <CateAddForm addCategory = {addCategory}/>} */}
+      {renderCategories()}
     </div>
-  )
-}
+  );
+};
 
 export default Categories;
