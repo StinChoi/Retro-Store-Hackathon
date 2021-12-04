4246e772e6cd5f523f78d47b9e57abeeecf2a54e
Fabio Xavier <fabio.g.xavier33@gmail.com>
2021-12-04 11:28:01 -0700

    jobs controller done and tested

--- app/controllers/api/jobs_controller.rb
+++ app/controllers/api/jobs_controller.rb
@@ -1,2 +1,44 @@
 class Api::JobsController < ApplicationController
+
+    before_action :set_job, only: [:destroy, :show, :update]
+    
+    def index
+        render json: Job.all
+    end
+
+    def create
+        @job = Job.new(set_params)
+        if @job.save
+            render json: @job
+        else
+            render json: {errors: @job.errors}, status: 422 # :unprocessable_entity
+        end
+    end
+
+    def show
+        render json: @job
+    end
+
+    def update
+        if @job.update(set_params)
+            render json: @job
+        else
+            render json: {errors: @job.errors}, status: 422 
+        end
+    end
+
+    def destroy
+        render json: @job.destroy
+    end
+
+    private
+
+    def set_params
+        params.require(:job).permit(:title, :company, :salary)
+    end
+
+    def set_job
+        @job = Job.find(params[:id])
+    end
+
 end
