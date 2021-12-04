0be6d279040237dd8f2a22f4d80981411cafbd32
Fabio Xavier <fabio.g.xavier33@gmail.com>
2021-12-04 14:22:09 -0700

    added back link from item

--- client/src/components/Item.js
+++ client/src/components/Item.js
@@ -1,11 +1,19 @@
 import React from "react";
+import { useLocation, useParams } from "react-router";
+import { Link } from "react-router-dom";
 
-const Item = (props) => {
+const Item = () => {
+
+  const location = useLocation();
+  const {item} = location.state;
+  const {category_id, id} = useParams();
+
   return (
     <div>
-      <p>Name: {props.name}</p>
-      <p>Price: {props.price}</p>
-      <p>Description: {props.Description}</p>
+      <Link to={`/categories/${category_id}/items`}>Back to category</Link>
+      <p>Name: {item.name}</p>
+      <p>Price: {item.price}</p>
+      <p>Description: {item.description}</p>
     </div>
   );
 };
