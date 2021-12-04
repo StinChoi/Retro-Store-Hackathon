fcdf0ef371ec43d1e0e8a58e3c6e4722edfd891c
Fabio Xavier <fabio.g.xavier33@gmail.com>
2021-12-04 10:38:28 -0700

    backend tested items&categories

--- app/controllers/api/items_controller.rb
+++ app/controllers/api/items_controller.rb
@@ -1,2 +1,49 @@
 class Api::ItemsController < ApplicationController
+
+    before_action :set_category
+    before_action :set_item, only: [:destroy, :show, :update]
+    
+    def index
+        render json: @category.items 
+    end
+
+    def create
+        @item = @category.items.new(set_params)
+        if @item.save
+            render json: @item
+        else
+            render json: {errors: @item.errors}, status: 422 
+        end
+    end
+
+    def show
+        render json: @item
+    end
+
+    def update
+        if @item.update(set_params)
+            render json: @item
+        else
+            render json: {errors: @item.errors}, status: 422 
+        end
+    end
+
+    def destroy
+        render json: @item.destroy
+    end
+
+    private
+
+    def set_params
+        params.require(:item).permit(:name, :description, :price)
+    end
+
+    def set_item
+        @item = @category.items.find(params[:id]) 
+    end
+
+    def set_category
+        @category = Category.find(params[:category_id])
+    end
+
 end
